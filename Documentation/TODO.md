4f56e03bd09516df4038d9ffed020042c9328ea7
rik <rikverhoeven10@gmail.com>
2024-12-03 13:09:32 +0100

    added microros agent setup

---
+++ Documentation/TODO.md
@@ -0,0 +1,14 @@
+
+
+# TODO
+
+
+
+
+
+
+# BUGS
+slider, not all sent values seen by esp32
+
+
+
---
+++ microros_ws/setup.sh
@@ -0,0 +1,14 @@
+# Update dependencies using rosdep
+sudo apt update && rosdep update
+rosdep install --from-paths src --ignore-src -y
+
+# Build micro-ROS tools and source them
+colcon build
+source install/local_setup.bash
+
+# Download micro-ROS-Agent packages
+ros2 run micro_ros_setup create_agent_ws.sh
+
+# Build step
+ros2 run micro_ros_setup build_agent.sh
+source install/local_setup.bash
